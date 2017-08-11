35e1a311a1ae615f5316e54d31927f3cd6d6fd84
Xu Cheng <git@xuc.me>
2017-08-11 15:41:26 +0800

    tweak popper js path

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,5 +1,5 @@
 //= require jquery/dist/jquery
-//= require popper.js/dist/popper
+//= require popper.js/dist/umd/popper
 //= require bootstrap/dist/js/bootstrap
 //= require katex
 //= require google_analytics
