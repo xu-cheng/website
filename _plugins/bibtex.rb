9aa29da59672360691872b7f4291a45e683da1e7
Xu Cheng <git@xuc.me>
2018-04-30 04:53:15 +0800

    fix

--- _plugins/bibtex.rb
+++ _plugins/bibtex.rb
@@ -3,7 +3,7 @@ require "pandoc-ruby"
 
 class Pandoc < BibTeX::Filter
   def apply(value)
-    html = PandocRuby.convert(value.to_s, :katex, from: :latex, to: :html)
+    html = PandocRuby.convert(value.to_s, :mathjax, from: :latex, to: :html)
     if html.scan("<p>").size == 1
       html.gsub!(%r{</?p>}, "")
     end
