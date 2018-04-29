0cd023c3608b28a7651c0a2a42c617fd27ca7d6f
Xu Cheng <git@xuc.me>
2018-04-30 05:20:16 +0800

    speed up

--- _plugins/bibtex.rb
+++ _plugins/bibtex.rb
@@ -3,7 +3,9 @@ require "pandoc-ruby"
 
 class Pandoc < BibTeX::Filter
   def apply(value)
-    html = PandocRuby.convert(value.to_s, :mathjax, from: :latex, to: :html)
+    value = value.to_s
+    return value unless %w[\ { }].any? { |c| value.include?(c) }
+    html = PandocRuby.convert(value, :mathjax, from: :latex, to: :html)
     if html.scan("<p>").size == 1
       html.gsub!(%r{</?p>}, "")
     end
