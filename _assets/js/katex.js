905e8874c6f307c0774184ec24a31f63709d7fbf
Xu Cheng <git@xuc.me>
2018-04-30 04:50:03 +0800

    fix katex

--- _assets/js/katex.js
+++ _assets/js/katex.js
@@ -2,6 +2,16 @@
 
 // render latex
 $(function() {
+    $("span.math.inline").replaceWith(function() {
+        var tex = $(this).text().replace(/^\\\(/, "").replace(/\\\)$/, "");
+        return katex.renderToString(tex);
+    });
+    $("span.math.display").replaceWith(function() {
+        var tex = $(this).text().replace(/^\\\[/, "").replace(/\\\]$/, "");
+        return katex.renderToString(tex, {
+            displayMode: true
+        });
+    });
     $("script[type='math/tex']").replaceWith(function() {
         return katex.renderToString($(this).text());
     });
--- _plugins/bibtex.rb
+++ _plugins/bibtex.rb
@@ -3,14 +3,10 @@ require "pandoc-ruby"
 
 class Pandoc < BibTeX::Filter
   def apply(value)
-    html = PandocRuby.convert(value.to_s, :gladtex, from: :latex, to: :html)
+    html = PandocRuby.convert(value.to_s, :katex, from: :latex, to: :html)
     if html.scan("<p>").size == 1
       html.gsub!(%r{</?p>}, "")
     end
-    html.gsub!('<EQ ENV="math">', '<script type="math/tex">')
-    html.gsub!('<EQ ENV="displaymath">', '<script type="math/tex; mode=display">')
-    html.gsub!('</EQ>', '</script>')
-    html.strip!
-    html
+    html.strip
   end
 end
