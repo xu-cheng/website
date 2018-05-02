a83d71e5370ec4176e07f1bb79faa4933111ce49
Xu Cheng <git@xuc.me>
2018-05-02 23:05:17 +0800

    add copy-tex

--- _assets/js/katex.js
+++ _assets/js/katex.js
@@ -1,6 +1,6 @@
 //= require katex/dist/katex
 
-// render latex
+// render LaTeX equation
 $(function() {
     $("span.math").replaceWith(function() {
         var tex = $(this).text();
@@ -29,4 +29,26 @@ $(function() {
             displayMode: true
         });
     });
+});
+
+// copy LaTeX equation
+$(document).on("copy", function(event) {
+    event = event.originalEvent;
+    var selection = window.getSelection();
+    if (selection.isCollapsed) { return; }
+    var fragment = $(selection.getRangeAt(0).cloneContents());
+    if (!fragment.find(".katex, .katex-equation")) { return; }
+    var html = fragment.clone();
+    html.find(".katex-mathml").remove();
+    event.clipboardData.setData("text/html", $("<div>").append(html).get(0).innerHTML);
+    var text = fragment;
+    text.find(".katex-html").remove();
+    text.find(".katex-display .katex-mathml").replaceWith(function() {
+        return "\\[" + $(this).find("annotation").text() + "\\]";
+    });
+    text.find(".katex-mathml").replaceWith(function() {
+        return "\\(" + $(this).find("annotation").text() + "\\)";
+    });
+    event.clipboardData.setData("text/plain", text.get(0).textContent);
+    event.preventDefault();
 });
