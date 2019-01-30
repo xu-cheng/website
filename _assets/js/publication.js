89ea41620ac948dcf04e58c56c511d513054ca06
Xu Cheng <git@xuc.me>
2019-01-30 15:19:16 +0800

    fix issue for cite btn

--- _assets/js/publication.js
+++ _assets/js/publication.js
@@ -1,6 +1,6 @@
 // copy citation
 $(function() {
-    $("button[id^=bibtex_btn\\:]").each(function() {
+    $("button[id^=bibtex_btn_]").each(function() {
         var id = $(this).attr("id");
         $(this).click(function() {
             var textarea_id = id.replace(/^bibtex_btn/, "bibtex");
