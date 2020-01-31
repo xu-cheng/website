afb589e6d8b957701fbb40c0ac434fc10d64741a
Cheng XU <git@xuc.me>
2020-01-31 21:16:52 +0800

    fix disqus

--- _assets/js/disqus.js
+++ _assets/js/disqus.js
@@ -1,7 +1,10 @@
-var disqus_config = function () {
+import $ from 'jquery';
+
+window.disqus_config = function () {
     this.page.url = $('meta[name=DISQUS_PAGE_URL]').attr("content");
     this.page.identifier = $('meta[name=DISQUS_PAGE_ID]').attr("content");
 };
+
 (function() {
 var d = document, s = d.createElement('script');
 s.src = 'https://xuc.disqus.com/embed.js';
