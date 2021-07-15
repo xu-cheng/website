e6d74556a014dd55f7f8c9e358bd3e529356e968
Cheng XU <git@xuc.me>
2021-07-15 13:44:20 -0700

    fix abstract rendering

--- _plugins/bibtex.rb
+++ _plugins/bibtex.rb
@@ -6,7 +6,7 @@ require "pandoc-ruby"
 class Pandoc < BibTeX::Filter
   def apply(value)
     value = value.to_s
-    return value unless %w[\ { } $].any? { |c| value.include?(c) }
+    return value unless %w[\\ { } $ % ~].any? { |c| value.include?(c) }
     html = PandocRuby.convert(value, :mathjax, from: :latex, to: :html)
     html.gsub!(%r{</?p>}, "") if html.scan("<p>").size == 1
     html.strip
