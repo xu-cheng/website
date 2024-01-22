a7fd56e050f45c3e3c9482e5ff90d37087336305
Cheng XU <git@xuc.me>
2024-01-21 18:36:28 -0800

    update webpack pipeline

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,93 @@
-const path = require('path');
-const is_prod = process.env.JEKYLL_ENV === 'production';
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const is_prod = process.env.JEKYLL_ENV === "production";
 
 var config = {
-    mode: is_prod ? 'production' : 'development',
+    mode: is_prod ? "production" : "development",
+    plugins: [new MiniCssExtractPlugin()],
     entry: {
-        'main': [
-            './node_modules/@popperjs/core/dist/umd/popper.js',
-            './node_modules/bootstrap/dist/js/bootstrap.js',
-            './_assets/js/katex.js',
-            './_assets/js/publication.js',
-            './_assets/js/disqus.js',
+        main: [
+            "./node_modules/@popperjs/core/dist/umd/popper.js",
+            "./node_modules/bootstrap/dist/js/bootstrap.js",
+            "./_assets/js/katex.js",
+            "./_assets/js/publication.js",
+            "./_assets/js/disqus.js",
         ],
-        'style': [
-            './_assets/css/main.scss',
-            './_assets/css/noscript.scss',
-        ],
-        'image': [
-            './_assets/img/photo.jpg',
-            './_assets/img/photo@2x.jpg',
-        ],
+        style: ["./_assets/css/main.scss"],
+        noscript: ["./_assets/css/noscript.scss"],
+        image: ["./_assets/img/photo.jpg", "./_assets/img/photo@2x.jpg"],
     },
     output: {
-        path: path.resolve(__dirname, 'assets'),
-        filename: '[name].js',
+        path: path.resolve(__dirname, "assets"),
+        filename: "[name].js",
     },
     module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [{
-                    loader: 'file-loader',
-                    options: {
-                        esModule: false,
-                        name: '[name].css',
-                        publicPath: '/assets/',
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            esModule: false,
+                            publicPath: "/assets/",
+                        },
                     },
-                },
-                {
-                    loader: 'extract-loader',
-                },
-                {
-                    loader: 'css-loader',
-                    options: {
-                        esModule: false,
-                        sourceMap: !is_prod,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            esModule: false,
+                            sourceMap: !is_prod,
+                        },
                     },
-                },
-                {
-                    loader: 'resolve-url-loader',
-                    options: {
-                        sourceMap: !is_prod,
+                    {
+                        loader: "resolve-url-loader",
+                        options: {
+                            sourceMap: !is_prod,
+                        },
                     },
-                },
-                {
-                    loader: 'sass-loader',
-                    options: {
-                        sourceMap: true,
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                        },
                     },
-                },
-            ],
-        }, {
-            test: /(\.(eot|otf|ttf|woff2?)|-webfont\.svg)$/,
-            use: [{
-                loader: 'file-loader',
-                options: {
-                    esModule: false,
-                    name: '[name].[ext]',
-                    outputPath: 'fonts/',
-                    publicPath: '/assets/fonts/',
-                },
-            }, ],
-        }, {
-            test: /\.jpg$/,
-            use: [{
-                loader: 'file-loader',
-                options: {
-                    esModule: false,
-                    name: '[name].[ext]',
-                    publicPath: '/assets/',
-                },
-            }, ],
-        }, ],
+                ],
+            },
+            {
+                test: /(\.(eot|otf|ttf|woff2?)|-webfont\.svg)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            esModule: false,
+                            name: "[name].[ext]",
+                            outputPath: "fonts/",
+                            publicPath: "/assets/fonts/",
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.jpg$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            esModule: false,
+                            name: "[name].[ext]",
+                            publicPath: "/assets/",
+                        },
+                    },
+                ],
+            },
+        ],
     },
 };
 
 module.exports = (env, argv) => {
     if (!is_prod) {
-        config.devtool = 'source-map';
+        config.devtool = "source-map";
     }
     return config;
 };
